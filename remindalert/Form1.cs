687b4ada5910c4086a86578c038aa5b9c51ce5f5
fireman <fireman4944@gmail.com>
2021-07-09 13:35:01 +0900

    3日目 完成？

--- remindalert/Form1.cs
+++ remindalert/Form1.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Configuration;
-using System.Diagnostics;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -12,6 +11,9 @@ namespace remindalert
         public Form1()
         {
             InitializeComponent();
+
+            //シャットダウン検知開始
+            SystemEvents.SessionEnding += new SessionEndingEventHandler(SystemEvents_SessionEnding);
 
             // 常駐セットアップ
             NotifyIcon icon = new NotifyIcon();
@@ -25,38 +27,29 @@ namespace remindalert
         private void Form1_Load(object sender, EventArgs e)
         {
             // タイマーで1分ごとにtimer1_Tickを実行
-            Text = Application.ProductName;
             timer1.Interval = 60000;
             timer1.Enabled = true;
+
         }
 
-
 #pragma warning disable CA1416 // プラットフォームの互換性を検証
+        //シャットダウン検知してポップアップ表示
         private void SystemEvents_SessionEnding(object sender, SessionEndingEventArgs e)
         {
             if (e.Reason == SessionEndReasons.Logoff
                 || e.Reason == SessionEndReasons.SystemShutdown)
             {
-                if (MessageBox.Show("体調チェックシートの入力は終わりましたか？",
-                "質問", MessageBoxButtons.YesNo) == DialogResult.No)
-                {
-                    //シャットダウンをキャンセルする
-                    e.Cancel = true;
-                    //アンケートへアクセス後、終了
-                    Process.Start(ConfigurationManager.AppSettings.Get("sheetUrl"));
-                    Application.Exit();
-                }
-                else
-                {
-                    //終了
-                    Application.Exit();
-                }
+                //シャットダウンをキャンセルする
+                e.Cancel = true;
+                RegularConfirmation rc = new RegularConfirmation();
+                rc.ShowQuestion(MessageBoxButtons.YesNo);
             }
         }
+#pragma warning restore CA1416 // プラットフォームの互換性を検証
 
+        //毎分設定された時間を見てあってたらポップアップ表示
         private void timer1_Tick(object sender, EventArgs e)
         {
-
             DateTime dt = DateTime.Now;
             string[] times = ConfigurationManager.AppSettings.Get("time").Split(',');
 
@@ -65,35 +58,9 @@ namespace remindalert
                 if (dt.Hour + ":" + dt.Minute == item)
                 {
                     RegularConfirmation rc = new RegularConfirmation();
-                    rc.showQuestion();
+                    rc.ShowQuestion(MessageBoxButtons.YesNoCancel);
                 }
             }
         }
-#pragma warning restore CA1416 // プラットフォームの互換性を検証
     }
-
-    public class RegularConfirmation
-    {
-        //指定時間到達
-        public void showQuestion()
-        {
-            switch (MessageBox.Show("体調チェックシートの入力は終わりましたか？",
-                "質問", MessageBoxButtons.YesNoCancel))
-            {
-                case DialogResult.Yes:
-                    //終了
-                    Application.Exit();
-                    break;
-
-                case DialogResult.No:
-                    //アンケートへアクセス
-                    Process.Start(ConfigurationManager.AppSettings.Get("sheetUrl"));
-                    break;
-
-                case DialogResult.Cancel:
-                    break;
-            }
-        }
-    }
-
 }
---
+++ remindalert/RegularConfirmation.cs
@@ -0,0 +1,32 @@
+﻿using System.Configuration;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace remindalert
+{
+    public class RegularConfirmation
+    {
+        //指定時間到達
+        public void ShowQuestion(MessageBoxButtons buttonStyle)
+        {
+            switch (MessageBox.Show("体調チェックシートの入力は終わりましたか？",
+                "質問", buttonStyle))
+            {
+                case DialogResult.Yes:
+                    //終了
+                    Application.Exit();
+                    break;
+
+                case DialogResult.No:
+                    //アンケートへアクセス
+                    Process.Start(ConfigurationManager.AppSettings.Get("sheetUrl"));
+                    Application.Exit();
+                    break;
+
+                case DialogResult.Cancel:
+                    break;
+            }
+        }
+    }
+
+}
