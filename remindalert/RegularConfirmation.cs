b42cb0020c141a35b1ea58106863173b1f49d745
fireman-git <fireman@gmail.com>
2021-07-19 14:03:52 +0900

    メッセージBOXを最前面に表示

--- remindalert/RegularConfirmation.cs
+++ remindalert/RegularConfirmation.cs
@@ -9,21 +9,27 @@ namespace remindalert
         //指定時間到達
         public void ShowQuestion(MessageBoxButtons buttonStyle)
         {
-            switch (MessageBox.Show("体調チェックシートの入力は終わりましたか？",
-                "体調チェックシート記入忘れ防止アラーム", buttonStyle))
+            // 親フォームを作成
+            using (Form f = new Form())
             {
-                case DialogResult.Yes:
-                    //終了
-                    Application.Exit();
-                    break;
+                f.TopMost = true; // 親フォームを常に最前面に表示する
+                switch (MessageBox.Show("体調チェックシートの入力は終わりましたか？",
+                "体調チェックシート記入忘れ防止アラーム", buttonStyle))
+                {
+                    case DialogResult.Yes:
+                        //終了
+                        Application.Exit();
+                        break;
 
-                case DialogResult.No:
-                    //アンケートへアクセス後終了
-                    Process.Start(ConfigurationManager.AppSettings.Get("sheetUrl"));
-                    break;
+                    case DialogResult.No:
+                        //アンケートへアクセス後終了
+                        Process.Start(ConfigurationManager.AppSettings.Get("sheetUrl"));
+                        break;
 
-                case DialogResult.Cancel:
-                    break;
+                    case DialogResult.Cancel:
+                        break;
+                }
+                f.TopMost = false;
             }
         }
     }
